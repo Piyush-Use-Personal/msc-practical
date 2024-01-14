69075fa8aa38c49743280c3cdc1b7761289bb2ab
Piyush dubey <piyush115dubey@gmail.com>
2024-01-14 09:39:28 +0530

    soft computing - p1

---
+++ soft-computing/practical-one/calculate-output.py
@@ -0,0 +1,24 @@
+print('app started!')
+
+n = int(input('Enter number of elements'))
+
+print('Enter elements')
+
+inputs = []
+
+for i in range(0, n):
+    ele = float(input())
+    inputs.append(ele)
+
+print(inputs)
+
+print('Enter weights')
+
+weights = []
+
+for i in range(0, n):
+    ele = float(input())
+    inputs.append(ele)
+
+print(inputs)
+
